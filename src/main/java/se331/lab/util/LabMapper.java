a4611e38521d3fbc6c459f18db73fb8a68ca2c97
ApiLies <Apisit_Liesmann@cmu.ac.th>
2025-10-26 19:23:38 +0700

    Remove Events related files

--- src/main/java/se331/lab/util/LabMapper.java
+++ src/main/java/se331/lab/util/LabMapper.java
@@ -2,10 +2,7 @@ package se331.lab.util;
 
 import java.util.List;
 import org.mapstruct.Mapper;
-
 import org.mapstruct.factory.Mappers;
-import se331.lab.entity.Event;
-import se331.lab.entity.EventDTO;
 import se331.lab.entity.News;
 import se331.lab.entity.NewsDto;
 
@@ -13,11 +10,7 @@ import se331.lab.entity.NewsDto;
 public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
 
-    // Map Event -> EventDTO but ignore eventHistory in participants to prevent recursion
-    EventDTO getEventDto(Event event);
-    List<EventDTO> getEventDto(List<Event> events);
-
-    // Existing event mappings…
-    List<NewsDto> getNewsDto(List<News> newsList);
+    // Map News -> NewsDto
     NewsDto getNewsDto(News news);
+    List<NewsDto> getNewsDto(List<News> newsList);
 }
